a4fab527d6f71b019bfa02bba7b76c03b4aaf63b
Brendan Kirby <bkirby@bntky.com>
2018-06-01 18:19:50 -0700

    Make all the timesheets tests pass

--- server/employees.js
+++ server/employees.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
+const timesheetsRouter = require('./timesheets');
 
 const db = new sqlite3.Database(
     process.env.TEST_DATABASE || './database.sqlite');
@@ -113,5 +114,7 @@ employeesRouter.delete('/:employeeId', (req, res, next) => {
         });
     });
 });
+
+employeesRouter.use('/:employeeId/timesheets', timesheetsRouter);
 
 module.exports = employeesRouter;
