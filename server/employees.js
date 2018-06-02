9ab7713da2b7c36a7f6b074f31faffa33ec89d5b
Brendan Kirby <bkirby@bntky.com>
2018-06-01 17:10:20 -0700

    Get the /api/employees/:employeeId tests to pass

--- server/employees.js
+++ server/employees.js
@@ -15,4 +15,103 @@ employeesRouter.get('/', (req, res, next) => {
     });
 });
 
+const isValidEmployee = employee => ['name', 'position', 'wage']
+      .every(employee.hasOwnProperty.bind(employee));
+
+const dbEmplVars = employee => { return {
+    $name: employee.name,
+    $position: employee.position,
+    $wage: employee.wage,
+    $is_current_employee: employee.is_current_employee === 0 ? 0 : 1
+}};
+
+employeesRouter.post('/', (req, res, next) => {
+    if( ! isValidEmployee(req.body.employee) ) {
+        return res.status(400).send(`ERROR: Missing fields for employee`);
+    }
+
+    const insertSQL = 'INSERT INTO Employee ' +
+          '(name, position, wage, is_current_employee) VALUES ' +
+          '($name, $position, $wage, $is_current_employee)';
+    const newEmpSQL = 'SELECT * FROM Employee WHERE id = $id';
+    db.run(insertSQL, dbEmplVars(req.body.employee), function(error) {
+        if(error) {
+            return res.status(500).send(`ERROR: Failed to insert new Employee: ${error}`);
+        }
+        db.get(newEmpSQL, {$id: this.lastID}, (error, row) => {
+            if(error) {
+                return res.status(500).send(`ERROR: Failed to find newly inserted Employee: ${error}`);
+            }
+            return res.status(201).send({employee: row});
+        });
+    });
+});
+
+employeesRouter.param('employeeId', (req, res, next, employeeId) => {
+    const emplSQL = 'SELECT * FROM Employee WHERE id = $id';
+    db.get(emplSQL, {$id: employeeId}, (error, row) => {
+        req.employee = row;
+        req.employeeId = employeeId;
+        next();
+    });
+});
+
+employeesRouter.param('employeeId', (req, res, next, employeeId) => {
+    if( ! req.employee ) {
+        return res.status(404).send(`ERROR: No employee found with id: ${req.employeeId}`);
+    }
+    next();
+});
+
+employeesRouter.get('/:employeeId', (req, res, next) => {
+    res.send({employee: req.employee});
+});
+
+employeesRouter.put('/:employeeId', (req, res, next) => {
+    if( ! isValidEmployee(req.body.employee) ) {
+        return res.status(400).send(`ERROR: Missing fields for employee`);
+    }
+
+    const updateSQL = 'UPDATE Employee set ' +
+          'name = $name, ' +
+          'position = $position, ' +
+          'wage = $wage, ' +
+          'is_current_employee = $is_current_employee ' +
+          'WHERE id = $id';
+    const newEmpSQL = 'SELECT * FROM Employee WHERE id = $id';
+    let vars = dbEmplVars(req.body.employee);
+    vars.$id = req.employeeId;
+    
+    db.run(updateSQL, vars, function(error) {
+        if(error) {
+            return res.status(500).send(`ERROR: Failed to insert new Employee: ${error}`);
+        }
+        db.get(newEmpSQL, {$id: req.employeeId}, (error, row) => {
+            if(error) {
+                return res.status(500).send(`ERROR: Failed to find newly inserted Employee: ${error}`);
+            }
+            return res.status(200).send({employee: row});
+        });
+    });
+});
+
+employeesRouter.delete('/:employeeId', (req, res, next) => {
+    const updateSQL = 'UPDATE Employee set ' +
+          'is_current_employee = 0 ' +
+          'WHERE id = $id';
+    const newEmpSQL = 'SELECT * FROM Employee WHERE id = $id';
+    
+    db.run(updateSQL, {$id: req.employeeId}, function(error) {
+        if(error) {
+            return res.status(500).send(`ERROR: Failed to insert new Employee: ${error}`);
+        }
+        db.get(newEmpSQL, {$id: req.employeeId}, (error, row) => {
+            if(error) {
+                return res.status(500).send(`ERROR: Failed to find newly inserted Employee: ${error}`);
+            }
+            return res.status(200).send({employee: row});
+        });
+    });
+});
+
 module.exports = employeesRouter;
